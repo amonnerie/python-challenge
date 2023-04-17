f689d21ffdc2c5c88090ec6b007d6487fb2dd81c
amonnerie <monnerie.andrea@gmail.com>
2023-04-17 13:58:29 -0700

    Organized with prettier notes

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -5,7 +5,7 @@
 import os
 import csv
 
-#initialize any variables
+#declare and initialize any variables
 voteList = []
 
 #open and read the file
@@ -14,21 +14,30 @@ csvpath = os.path.join( 'Resources', 'election_data.csv')
 #receive the data from file to list
 with open(csvpath) as csvopen:
     voteFile = csv.reader(csvopen, delimiter=',')
-    header = next(voteFile) #header is stored but excluded
+    
+    #header is stored but excluded
+    header = next(voteFile) 
 
+    #from data from file to a 2d list
     for row in voteFile:
         voteList.append(row)
-csvopen.close() #don't need the file anymore
+
+#don't need the file anymore
+csvopen.close() 
 
 #Part 2 - analysis/math ---------------------------------------
 
 #The total number of votes cast
 totalVotes = len(voteList)
-#print(totalVotes) good
+
+
 #A complete list of candidates who received votes
+#declare and initialize needed variables
 StockhamVote = 0
 DeGetteVote = 0
 DoaneVote = 0
+
+#count according to the voted candidate
 for vote in voteList:
     if vote[2] == "Charles Casper Stockham":
         StockhamVote += 1
@@ -37,9 +46,9 @@ for vote in voteList:
     elif vote[2] == "Raymon Anthony Doane":
         DoaneVote += 1
     else:
-        print("Unknown Candidate")
+        #in case the voter put someone other than the candidates above
+        print(f"Voter ID {voteList[vote][0]}: Unknown Candidate")
         break
-#print(f"{StockhamVote} {DeGetteVote} {DoaneVote}")
 
 #get percentages
 StockhamPer = round((StockhamVote/totalVotes)*100, 3)
@@ -47,7 +56,10 @@ DeGettePer = round((DeGetteVote/totalVotes)*100,3)
 DoanePer = round((DoaneVote/totalVotes)*100, 3)
 
 #The winner of the election based on popular vote
+#declare and initialized the needed variable
 winner = ""
+
+#conditionals to determine the winner 
 if StockhamVote > DeGetteVote:
     winner = "Charles Casper Stockham"
 elif DeGetteVote > DoaneVote:
@@ -57,9 +69,8 @@ elif DoaneVote > StockhamVote:
 else:
     #in case the candidates were all a tie
     winner = "N/A: Tie Election"
-print(winner)
 
-#part 3 - print to console -------------------------------------
+#Part 3 - print to console -------------------------------------
 result = "Election Results" + "\n\n-------------------------"
 result = result + f"\n\nTotal Votes: {totalVotes}"
 result = result + "\n\n-------------------------"
@@ -70,12 +81,16 @@ result = result + "\n\n-------------------------"
 result = result + f"\n\nWinner: {winner}"
 result = result + "\n\n-------------------------"
 print(result)
-#part 4 - write output file ------------------------------------
 
+#Part 4 - write output file ------------------------------------
+
+#set up the output file
 textpath = os.path.join("analysis", "ElectionResult.txt")
 
 with open(textpath, 'w') as outputFile:
 
     # write the result in File
     outputFile.write(result)
+
+#done with the file
 outputFile.close()
